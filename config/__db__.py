2322d8e4b37d4a311bf421cdcc70f4d9dad6dbe2
Fabrizio Coccetti <fabrizio.coccetti@centrofermi.it>
2014-12-12 11:05:21 +0100

    Database settings

---
+++ config/__db__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 12 10:58:33 2014
+
+@author: Fabrizio Coccetti (fabrizio.coccetti@centrofermi.it) [www.fc8.net]
+"""
+
+EEE_RUNDB2_CONF = {
+    'host': '127.0.0.1',
+    'user': 'eee',
+    'dbname': 'eee_rundb2',
+    'pwd': ''
+    }
