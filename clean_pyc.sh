4675cf2a48518aceedd562d60113eead5dcaf5d6
Fabrizio Coccetti <fabrizio.coccetti@centrofermi.it>
2015-02-17 14:58:59 +0100

    cleaning

--- clean_pyc.sh
+++ clean_pyc.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 #
-# Delete Python's compiled *.pyc and __pycache__ files like a pro
-# https://gist.github.com/jakubroztocil/7892597
-#
 # Usage:
-# Delele *.pyc and __pycache__ files recursively in the current directory:
+# Delele *.pyc *py-e and __pycache__ files recursively in the current directory:
 # $ pyc  
 # 
-# The same, but under /path:
-# $ pyc /path
-#    
  
 if [ "$1" ]; then
     WHERE="$1"
@@ -20,4 +14,6 @@ fi
 find "$WHERE" \
     -name '__pycache__' -delete -print \
     -o \
-    -name '*.pyc' -delete -print
+    -name '*.pyc' -delete -print \
+    -o \
+    -name '*.py-e' -delete -print
