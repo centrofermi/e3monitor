ac942a49775acac09d72b270f02d414d7019de6f
Fabrizio Coccetti <fabrizio.coccetti@centrofermi.it>
2015-02-28 23:47:16 +0100

    change in comments

--- apps/e3cnaf.py
+++ apps/e3cnaf.py
@@ -20,6 +20,10 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# ****************************************************************
+# This software is not in use anymore
+# since we set up BTSync on all EEE Telescopes (jan 2015)
 
 import logging
 import logging.config
