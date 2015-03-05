97edf58f38efd93df8fbac82322bc02438f363ad
Fabrizio Coccetti <fabrizio.coccetti@centrofermi.it>
2015-03-05 18:08:43 +0100

    minor

--- tasks/set_version.py
+++ tasks/set_version.py
@@ -3,6 +3,7 @@
 Created on Thu Nov 20 11:40:55 2014
 
 @author: Fabrizio Coccetti
+Return string with e3monitor version and time/date
 """
 from datetime import datetime
 from e3monitor.__version__ import TAG
