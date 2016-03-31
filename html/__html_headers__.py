8ec61c1cadb5a7e73b5499c844f0fa9e0c5232b6
Fabrizio Coccetti <fabrizio.coccetti@centrofermi.it>
2016-03-31 15:25:00 +0200

    html webpage language set to italian

--- html/__html_headers__.py
+++ html/__html_headers__.py
@@ -7,7 +7,7 @@ Created on Mon Nov 17 18:48:52 2014
 HEADER_HTML = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="it" xml:lang="it" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="author" content="Fabrizio Coccetti" />
