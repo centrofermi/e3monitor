6fd3c87fa10ed0e3456675e6a558a6f459b4341c
Fabrizio Coccetti <fabrizio.coccetti@centrofermi.it>
2017-01-19 16:29:43 +0100

    Run 0 to 2 labels and regions for the total track plot

--- apps/e3plotstat.py
+++ apps/e3plotstat.py
@@ -36,6 +36,8 @@ import logging
 import logging.config
 from matplotlib.dates import MonthLocator, WeekdayLocator, DateFormatter
 #import matplotlib.ticker as mtick
+import matplotlib.dates as md
+import matplotlib.patches as patches
 from matplotlib import ticker
 from datetime import datetime
 from e3monitor.tasks.read_pickle import read_pickle
@@ -53,6 +55,23 @@ ntracks = []
 logging.config.fileConfig(logConfigFile)
 logger = logging.getLogger('full')
 logger.info('Started: Make plot for Statistics')
+
+# Define dates of Runs
+# Pilot Run 2014
+# martedi 21 ottobre ore 12:00 inizio presa dati in modalita commissioning
+# lunedi 27 ottobre ore 12:00      inizio presa dati
+# venerdi 14 novembre ore 12:00   termine presa dati
+# Run 1 2015
+# 23-27 febbraio: Commissioning
+#27 febbraio - 30 aprile: Presa dati ufficiale
+dateRun0Start = md.date2num(datetime(2014,10,27))
+dateRun0End = md.date2num(datetime(2014,11,14))
+dateRun1Start = md.date2num(datetime(2015,2,27))
+dateRun1End = md.date2num(datetime(2015,4,30))
+dateRun2Start = md.date2num(datetime(2015,11,7))
+dateRun2End = md.date2num(datetime(2016,5,20))
+dateRun3Start = md.date2num(datetime(2016,11,1))
+dateRun3End = md.date2num(datetime(2017,6,30))
 
 # Read pickle
 trackStat = read_pickle(pathWorkDir, pklStatFile)
@@ -88,17 +107,30 @@ ax.set_ylabel("Number of tracks")
 ax.yaxis.set_major_locator(sci)
 ax.yaxis.set_major_formatter(sciFmt)
 
-# Color Runs
-#run3 = [datetime(2016, 9, 30, 0, 0), datetime(2016, 10, 31, 0, 0)]
-#ax.fill_between(dates, 0, ntracks, where=dates<datetime(2015,12,8), facecolor='#87CEFA')
-#ax.fill_between(dates, 0, ntracks, facecolor='#99FF99')
-
 # Print graph
 ax.plot_date(dates, ntracks, color='#0088CC', linestyle='dotted', marker='o')
-ax.fill_between(dates, 0, ntracks, facecolor='#87CEFA')
+ax.fill_between(dates, 0, ntracks, facecolor='#87CEFA',zorder=1)
 
 # Color Runs
+yMin, yMax = ax.get_ylim()
+hText = 7e9
+# Pilot Run
+#ax.add_patch(patches.Rectangle((dateRun0Start, yMin), dateRun0End-dateRun0Start, yMax, fill=1, facecolor="#c0392b", alpha=.3))
+ax.annotate('PILOT RUN', xy=(dateRun0Start-5+(dateRun0End-dateRun0Start)/2,hText+1e9),size=13,rotation='vertical',weight='bold',color='#c0392b', backgroundcolor='white',zorder=10)
+ax.add_patch(patches.Rectangle((dateRun0Start, yMin), dateRun0End-dateRun0Start, yMax, fill=0, facecolor="#c0392b", edgecolor="#c0392b", linestyle='dashed',linewidth=2,zorder=20))
+ax.annotate('', xy=(dateRun0End,hText), xytext=(dateRun0Start, hText), arrowprops=dict(arrowstyle='<->',connectionstyle="arc3,rad=0.0",edgecolor='#c0392b',linewidth=0.3,zorder=10))
+# Run 1
+ax.add_patch(patches.Rectangle((dateRun1Start, yMin), dateRun1End-dateRun1Start, yMax, fill=0, facecolor="#c0392b", edgecolor="#c0392b", linestyle='dashed',linewidth=2))
+ax.annotate('', xy=(dateRun1End,hText), xytext=(dateRun1Start, hText), arrowprops=dict(arrowstyle='<->',connectionstyle="arc3,rad=0.0",edgecolor='#c0392b',linewidth=2.5))
+ax.annotate('RUN 1', xy=(dateRun1Start-5+(dateRun1End-dateRun1Start)/2,hText+1e9),size=18,rotation='vertical',weight='bold',color='#c0392b', backgroundcolor='white')
+# Run 2
+ax.add_patch(patches.Rectangle((dateRun2Start, yMin), dateRun2End-dateRun2Start, yMax, fill=0, facecolor="#c0392b", edgecolor="#c0392b", linestyle='dashed',linewidth=2))
+ax.annotate('', xy=(dateRun2End,hText), xytext=(dateRun2Start, hText), arrowprops=dict(arrowstyle='<->',connectionstyle="arc3,rad=0.0",edgecolor='#c0392b',linewidth=2.5))
+ax.annotate('RUN 2', xy=(dateRun2Start-35+(dateRun2End-dateRun2Start)/2,hText+1e9),size=18,weight='bold',color='#c0392b', backgroundcolor='#87CEFA')
+# Run 3
+ax.add_patch(patches.Rectangle((dateRun3Start, yMin), dateRun3End-dateRun3Start, yMax, fill=0, facecolor="#c0392b", edgecolor="#c0392b", linestyle='dashed',linewidth=2))
 
+# Save plot
 fig.autofmt_xdate()
 fig.savefig(pathSaveFig + 'tracks.png')
 logger.info("Plot saved in " + str(pathSaveFig) + 'tracks.png')
