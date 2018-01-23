4f15707f609328dec0a76d9fcf01a8a3f9e9cf87
Francesco Noferini <noferini@bo.infn.it>
2018-01-23 12:34:26 +0100

    fix

--- coincDQM/copyOnWeb.sh
+++ coincDQM/copyOnWeb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp /home/analisi/coincDQM/*.png  /var/www/html/monitor/coincidences/.
+cp /home/analisi/coincDQM/*-*.html  /var/www/html/monitor/coincidences/.
 cp /home/analisi/coincDQM/coincidences.html  /var/www/html/monitor/.
